1a1ee4ef2d8f94d9dd5bcea8bb322d64821360d5
Mezerreg Houssam eddine <hou11am23@gmail.com>
2022-04-14 16:30:20 +0200

    some changes

--- js/script.js
+++ js/script.js
@@ -5,9 +5,14 @@ const action = document.getElementById("actions");
 menu.addEventListener("click",()=>{
     hundleMenu();
 });
-
+ 
 function hundleMenu(){
     menu.classList.toggle("is-active");
     action.classList.toggle("is-active"); 
     }
+
+action.addEventListener("click",()=>{
+    menu.classList.toggle("is-active");
+    action.classList.toggle("is-active"); 
     
+});
